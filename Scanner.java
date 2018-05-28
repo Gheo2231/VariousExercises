37655320d426914737c46a6a70827404fd497902
Gheorghe <iaconimgheorghe@gmail.com>
2018-05-28 17:38:15 +0300

    Update Scanner.java

--- Scanner.java
+++ Scanner.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
-class Scanner {
-    public static void main(String[ ] args) {
-    Scanner keyboardScan = new Scanner(System.in)
-       System.out.println("Enter name, please:");
-       String name = keyboardScan.nextLine();
+
+public class Scanner{
+    public static void main(String[] args){
+        Scanner keyboardScan = new Scanner(System.in);
+
+        System.out.println("Enter your name:" );
+        String name = keyboardScan.nextLine();
+        
         System.out.printf("Hello %s!", name);
+
     }
 }
